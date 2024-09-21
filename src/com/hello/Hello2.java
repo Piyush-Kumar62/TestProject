7c28bf93537b80144992fb888a9e144365c03a64
PIYUSH KUMAR <220101130066@cutm.ac.in>
2024-09-21 14:55:45 +0530

    added some changes in this file and print second line

--- src/com/hello/Hello2.java
+++ src/com/hello/Hello2.java
@@ -5,6 +5,8 @@ public class Hello2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello Coders");
+		System.out.println("hello coder and tell me something in this project");
+
 
 	}
 
